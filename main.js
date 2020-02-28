098c574511e0bb8a3fff33ba4d520b6c8217ee64
rollingspark@gmail.com <rollingspark@gmail.com>
2020-02-28 08:22:15 +0100

    Changed data-format

--- main.js
+++ main.js
@@ -26,20 +26,47 @@ if( fs.existsSync(fileName)){
 }
 
 const dayFile = smbPath + moment().format('YYYY-MM-DD') + '.json'
+/*
+Parameters:
+-----------
+                                          Unit                                      Description
+2:tre200s0                                  °C                                        Air temperature 2 m above ground; current value
+3:rre150z0                                  mm                                        Precipitation; ten minutes total
+4:sre000z0                                  min                                       Sunshine duration; ten minutes total
+gre000z0                                  W/m²                                      Global radiation; ten minutes mean
+ure200s0                                  %                                         Relative air humidity 2 m above ground; current value
+tde200s0                                  °C                                        Dew point 2 m above ground; current value
+dkl010z0                                  °                                         Wind direction; ten minutes mean
+9:fu3010z0                                  km/h                                      Wind speed; ten minutes mean
+10:fu3010z1                                  km/h                                      Gust peak (one second); maximum
+prestas0                                  hPa                                       Pressure at station level (QFE); current value
+pp0qffs0                                  hPa                                       Pressure reduced to sea level (QFF); current value
+pp0qnhs0                                  hPa                                       Pressure reduced to sea level according to standard atmosphere (QNH); current value
+ppz850s0                                  gpm                                       geopotential height of the 850 hPa-surface; current value
+ppz700s0                                  gpm                                       geopotential height of the 700 hPa-surface; current value
+dv1towz0                                  °                                         Wind direction vectoriel; ten minutes interval; tool 1 
+fu3towz0                                  km/h                                      Wind speed tower; ten minutes mean
+fu3towz1                                  km/h                                      Gust peak (one second) tower; maximum
+ta1tows0                                  °C                                        Air temperature tool 1
+uretows0                                  %                                         Relative air humidity tower; current value
+tdetows0                                  °C                                        Dew point tower
 
+LEI;202002132040;4.3;0.3;0;4;97;3.8;161;1.4;3.2;969.0;1010.4;1009.3;-;-;-;-;-;-;-;-
+
+*/
 class WetterDaten{
     constructor( f ){
         // this.time = f[1]
         this.air = f[2]
-        this.sun = f[3]
-        this.rain = f[4]
+        this.sun = f[4]
+        this.rain = f[3]
         this.windd = f[5]
-        this.winds = f[6]
-        this.windm = f[8]  
-        this.humidity = f[9]
-        this.airp1 = f[7]
-        this.airp2 = f[10]
-        this.airp3 = f[11]
+        this.winds = f[9]
+        this.windm = f[10]  
+        this.humidity = f[6]
+        this.airp1 = f[11]
+        this.airp2 = f[12]
+        this.airp3 = f[13]
         this.created = moment().format()
     }
 }
@@ -63,7 +90,7 @@ function saveDayFile(d){
 request.get(dotconf.metnet, function (r, data) {
     const lines = data.body.split('\n');
     const st = lines.filter((l) => { return l.match(/^LEI/); });
-    var d = new WetterDaten( st.pop().split("|") )
+    var d = new WetterDaten( st.pop().split(";") )
     const sensorPath= "/sys/bus/w1/devices/w1_bus_master1/28-03168acbaeff/w1_slave"
     if( fs.existsSync( sensorPath )){
         const innen = parseFloat( fs.readFileSync(sensorPath).toString().split('\n')[1].split('=')[1]/1000 ).toFixed(1)
